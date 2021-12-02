2425b7fe0af740e28c462103992e5deecba151ac
mechabubba <stevenvanniisprettycool@gmail.com>
2021-12-01 19:31:43 -0600

    country state and city names are now no longer shorthand programmer gibberish

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 # chumtoad
 
-func.zone discord bot. Invite [here.](https://discordapp.com/oauth2/authorize?client_id=636259214779613214&scope=bot&permissions=0)
+The func.zone discord bot. Invite [here.](https://discordapp.com/oauth2/authorize?client_id=636259214779613214&scope=bot&permissions=0)
+
+Steam country and state codes are sourced from [RudeySH/SteamCountries](https://github.com/RudeySH/SteamCountries/).
--- bot/commands/steam.js
+++ bot/commands/steam.js
@@ -3,12 +3,13 @@ const { MessageEmbed } = require("discord.js");
 const { DateTime } = require("luxon");
 const fetch = require("node-fetch");
 const SteamID = require("steamid");
+const { countries } = require("../../data/countries-states-cities.min.json");
 
 module.exports = [
     new CommandBlock({
         identity: ["steam", "steamid"],
         description: "Gets someones Steam profile information.",
-        usage: "[SteamID]"
+        usage: "[SteamID or custom URL]"
     }, async function(client, message, content, [id]) {
         const positive = client.config.get("metadata.reactions.positive").value();
         const negative = client.config.get("metadata.reactions.negative").value();
@@ -23,7 +24,13 @@ module.exports = [
         try {
             steamID = new SteamID(id);
         } catch(e) {
-            return message.channel.send(`<:_:${negative}> This is not a valid SteamID!`);
+            // check if its a vanity url via another api endpoint
+            const resp = await fetch(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=${apikey}&vanityurl=${id}`);
+            const json = await resp.json();
+            if(!json.response || json.response.success !== 1) {
+                return message.channel.send(`<:_:${negative}> This is not a valid SteamID or custom URL!`);
+            }
+            steamID = new SteamID(json.response.steamid);
         }
 
         if(!steamID.isValidIndividual()) return message.channel.send(`<:_:${negative}> This SteamID does not correspond to an individuals account!`);
@@ -45,25 +52,47 @@ module.exports = [
             `Status:      ${profile.personastate == 0 ? "Offline" : profile.personastate == 1 ? "Online" : profile.personastate == 2 ? "Busy" : profile.personastate == 3 ? "Away" : profile.personastate == 4 ? "Snooze" : profile.personastate == 5 ? "Looking to Trade" : profile.personastate == 6 ? "Looking to Play" : "Unknown"}\n`
         ].join("\n");
         
-        if(profile.communityvisibilitystate !== 3) { // private profile
+        // private profile
+        if(profile.communityvisibilitystate !== 3) {
             embed.setFooter("This profile is private.");
-        } else if(!profile.profilestate) { // no community profile
+
+        // no community profile
+        } else if(!profile.profilestate) {
             embed.setFooter("This user has not set up their Steam Community profile.");
-        } else if(profile.communityvisibilitystate === 3) { // public profile
+
+        // public profile
+        } else if(profile.communityvisibilitystate === 3) {
             // some of these aren't given in the api response.
             if(profile.realname) {
                 description += `Real Name:   ${profile.realname}\n`;
             }
+            
+            // location stuff
+            let country;
             if(profile.loccountrycode) {
-                description += `Country:     ${profile.loccountrycode}\n`;
+                country = countries.find((obj) => obj.code == profile.loccountrycode);
+                description += `Country:     ${country ? country.name : profile.loccountrycode}\n`;
+            }
+            let state;
+            if(profile.locstatecode) {
+                state = country.states.find((obj) => obj.code === profile.locstatecode);
+                description += `State:       ${state ? state.name : profile.locstatecode}\n`;
+            }
+            let city;
+            if(profile.loccityid) {
+                city = state.cities.find((obj) => obj.id === profile.loccityid);
+                description += `City:        ${city ? city.name : profile.loccityid}\n`;
             }
             description += `Created:     ${DateTime.fromMillis(profile.timecreated * 1000).toLocaleString(DateTime.DATETIME_MED)}\n`;
-            if(profile.personastate == 0 && profile.lastlogoff) { // unsure why this isn't given sometimes... but ok
+            
+            // for some reason this *only sometimes* is not included in the response.
+            if(profile.personastate == 0 && profile.lastlogoff) {
                 description += `Last online: ${DateTime.fromMillis(profile.lastlogoff * 1000).toLocaleString(DateTime.DATETIME_MED)}\n`;
             }
             embed.setFooter("This profile is public.");
         }
 
+        // calculate steam ids
         description = description + [
             `---`,
             `SteamID:     ${steamID.getSteam2RenderedID(true)}`,
@@ -73,6 +102,7 @@ module.exports = [
         description = description + `\`\`\``;
         embed.setDescription(description);
 
+        // launch
         return message.channel.send(embed);
     }),
 ];
