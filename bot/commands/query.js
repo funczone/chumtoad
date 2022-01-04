b3e9848f5616903f650198230c8198ae682b2949
mechabubba <stevenvanniisprettycool@gmail.com>
2022-01-03 18:07:58 -0600

    fix stupid ass query bug
    in a stupid ass way

--- bot/commands/query.js
+++ bot/commands/query.js
@@ -1,17 +1,20 @@
-const { query } = require("gamedig");
+const query = require("../../modules/source-server-query") // hacky fix
 const { MessageEmbed } = require("discord.js");
 const CommandBlock = require("../../modules/CommandBlock");
 
+const preview = {
+    location: "http://func.site.nfoservers.com/assets/mp",
+    extension: "jpg"
+};
+
 const autofill = {
-  "ttt": { ip: "ttt.func.zone", port: 27015, protocol: "garrysmod" },
-  "tf2": { ip: "tf2.func.zone", port: 27015, protocol: "tf2" },
-  "tftober": { ip: "tf2.func.zone", port: 27016, protocol: "tf2" }
+    "ttt": { vanity: "ttt.func.zone", ip: "74.91.124.160", port: 27015, game: "garrysmod" },
 }
 
 let description = "Querys source engine servers.\`\`\`markdown\n# AUTOFILL OPTIONS\nUse one of the strings in turquoise to quickly query one of our servers.\n";
 for(const key in autofill) {
-  let i = autofill[key];
-  description += `* [ ${key} ][ ${i.ip}${i.port ? ":" + i.port : ""} ]\n`;
+    let i = autofill[key];
+    description += `* [ ${key} ][ ${i.ip}${i.port ? ":" + i.port : ""} ]\n`;
 }
 description += "```";
 
@@ -24,7 +27,7 @@ module.exports = new CommandBlock({
     locked: false,
     clientPermissions: ["VIEW_CHANNEL", "SEND_MESSAGES"],
     userPermissions: null,
-  }, async function(client, message, content, [ip, port]) {
+}, async function(client, message, content, [ip, port]) {
     const online = client.config.get("metadata.reactions.online").value();
     const offline = client.config.get("metadata.reactions.offline").value();
     const negative = client.config.get("metadata.reactions.negative").value();
@@ -34,53 +37,52 @@ module.exports = new CommandBlock({
 
     let qi = autofill[ip];
     if(qi) {
-      ip = qi.ip;
-      port = qi.port;
+        ip = qi.ip;
+        port = qi.port;
     } else if(ip.match(/:/g)) {
-      let split = ip.split(":");
-      ip = split[0];
-      port = split[split.length - 1];
+        let split = ip.split(":");
+        ip = split[0];
+        port = split[split.length - 1];
     }
 
-    const vanity = ip + (port ? `:${port}` : "");
+    const vanity = (qi ? qi.vanity : ip) + (port ? `:${port}` : "");
     const embed = new MessageEmbed();
 
-    let info;
+    //let info;
     try {
-      info = await query({
-        type: qi.protocol || "protocol-valve",
-        host: ip,
-        port: port || "27015"
-      });
-    } catch(e) { 
-      embed.setTitle(vanity);
-      embed.setColor("#F04747")
-      embed.setFooter("This server is offline.", `https://cdn.discordapp.com/emojis/${offline}.png`);
-      return message.channel.send(embed);
+        const info = await query.info(ip, port, 2000);
+        const players = await query.players(ip, port, 2000);
+        query.destroy();
+
+        embed.setTitle(info.name);
+        embed.setColor("#43B581")
+        embed.addField(`Basic Info`, `IP: \`${vanity}\`\nConnect: steam://connect/${vanity}`);
+        embed.setFooter("This server is online!", `https://cdn.discordapp.com/emojis/${online}.png`);
+        
+        let _players = "";
+        if(players.length == 0) {
+            _players = "Dead server. :(";
+        } else {
+            players.sort((a, b) => (a.score < b.score) ? 1 : -1);
+            for(i = 0; i < players.length; i++) {
+                let ply = players[i];
+                _players += `${i + 1}. ${ply.name ? `${ply.name} (${ply.score})` : "Joining in..."}\n`;
+            }
+        }
+
+        embed.addField(`Current Players (${players.length} / ${info.maxplayers}${players.length >= info.maxplayers ? " - full!" : ``})`, _players);
+        embed.addField(`Current Map`, `\`${info.map}\``);
+
+        if(qi && qi.game === "garrysmod") {
+            embed.setImage(`${preview.location}/${info.map}.${preview.extension}`);
+        }
+
+        return message.channel.send(embed);
+    } catch(e) {
+        console.error(e);
+        embed.setTitle(vanity);
+        embed.setColor("#F04747")
+        embed.setFooter("This server is offline.", `https://cdn.discordapp.com/emojis/${offline}.png`);
+        return message.channel.send(embed);
     }
-
-    embed.setColor("#43B581")
-    embed.setFooter("This server is online!", `https://cdn.discordapp.com/emojis/${online}.png`);
-
-    embed.setTitle(info.name);
-    embed.addField(`Basic Info`, `IP: \`${vanity}\`\nConnect: steam://connect/${vanity}`);
-
-    let players = "";
-    info.players.sort((a, b) => (a.score < b.score) ? 1 : -1);
-    for(i = 0; i < info.players.length; i++) {
-      let ply = info.players[i];
-      players += `${i + 1}. ${ply.name ? `${ply.name} (${ply.score})` : "Joining in..."}\n`;
-    }
-    if(!players) {
-      players = "Dead server. :(";
-    }
-    embed.addField(`Current Players (${info.players.length} / ${info.maxplayers}${info.players.length >= info.maxplayers ? " - full!" : ``})`, players);
-    embed.addField(`Current Map`, `\`${info.map}\``);
-
-    if(qi && qi.protocol === "garrysmod") {
-        embed.setImage(`http://func.site.nfoservers.com/assets/mp/${info.map}.jpg`);
-    }
-
-    return message.channel.send(embed);
-  }
-);
+});
